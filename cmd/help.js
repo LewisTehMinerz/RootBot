28d7719b41d6d536e3769a82253f79b43a895758
Sascha_T <16324266+Sascha-T@users.noreply.github.com>
2017-12-03 17:48:21 +0100

    Edited help.js

--- cmd/help.js
+++ cmd/help.js
@@ -1,16 +1,8 @@
 exports.run = (bot, message, args = []) => {
 
-    message.author.send(
-        "```" +
-        ">help | Current Command" +
-        "\n>dm | Used to direct message users as the bot" +
-        "\n>eval | Used to issue console commands VIA chat." +
-        "\n>report | Used to report users or staff members" +
-        "\n>say | Used to say things" +
-        "\n>bean | Beans someone" +
-        "\n>note | Used to note users." +
-        "```"
-    )
+
+
+    message.author.send(bot.helptext + "```");
     message.react("📜");
 
     };
